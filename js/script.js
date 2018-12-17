102ebcde60bacad30d7e05daa9d36d65554c1ab5
Joanna Buchała <joanna.b2@op.pl>
2018-12-17 21:30:20 +0100

    add tags logic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-function open(ev, sectionName) {
+function openSection(event, sectionName) {
 	var i, link_content, link;
 
 	link_content = document.getElementsByClassName('link-content');
 	for (i = 0; i < link_content.length; i++) {
 		link_content[i].style.display = 'none';
 	}
+
+	link = document.getElementsByClassName('link');
+	for (i = 0; i < link.length; i++) {
+		link[i].className = link[i].className.replace(' active', '');
+	}
+
+	document.getElementById(sectionName).style.display = 'block';
+	event.currentTarget.className += ' active';
 }
+
+document.getElementById('defaultOpen').click();
+
